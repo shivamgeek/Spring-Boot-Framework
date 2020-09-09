b239eb87f8824daaa1ee7f1bc516854feef10431
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-09 14:52:22 +0530

    Add custom properties and use with @Value('')

--- src/main/java/com/shivam/rest/HelloRestController.java
+++ src/main/java/com/shivam/rest/HelloRestController.java
@@ -1,11 +1,21 @@
 package com.shivam.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
+	@Value("${yourname.firstname}")
+	String firstName;
+	
+	@Value("${yourname.lastname}")
+	String lastName;
+	
+	@Value("${yourname.age}")
+	String age;
+	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello from Spring Boot";
@@ -14,6 +24,11 @@ public class HelloRestController {
 	@GetMapping("/workout")
 	public String workout() {
 		return "Do WORKOUT atlest 5 day a week!!!!";
+	}
+	
+	@GetMapping("/yourname")
+	public String yourName() {
+		return "Data is "+firstName+" "+lastName+" - "+age;
 	}
 	
 }
