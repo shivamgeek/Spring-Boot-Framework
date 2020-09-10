d30549c582c6a04737785e32d0264a177d7f1b21
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-10 17:29:21 +0530

    Explicitly overriding default REST resource path

--- src/main/java/com/shivam/dao/EmployeeRepository.java
+++ src/main/java/com/shivam/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.shivam.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.shivam.Entity.Employee;
 
+@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
 	
 	/*
